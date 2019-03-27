488dca67f5399cbf5182a7baadef428d9064613f
Marcin Woźniak <wozniak.marcindneu@gmail.com>
2019-03-27 01:18:32 +0100

    Final version

---
+++ js/script.js
@@ -0,0 +1,105 @@
+class Stopwatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+    }
+
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+        this.print();
+        document.getElementById('start').classList.remove('active');
+        document.getElementById('stop').classList.remove('active');
+    }
+
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+
+    start() {
+        if(!this.running) {
+            this.running = true;
+            this.watch - setInterval(() => this.step(), 10);
+        }
+
+        document.getElementById('start').classList.add('active');
+        document.getElementById('stop').classList.remove('active');
+    }
+
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+        document.getElementById('stop').classList.add('active');
+        document.getElementById('start').classList.remove('active');
+    }
+
+    restart() {
+        this.reset();
+        this.print();
+    }
+
+    addCheckpoint() {
+        if(this.running) {
+            let currentCheckpoint = this.display.innerText;
+            let resultList = document.querySelector('.results');
+            resultList.insertAdjacentHTML('beforeend', `<li class="record">${currentCheckpoint}</li>`); 
+        }
+    }
+
+    resetRecordList() {
+        document.querySelector('.results').innerHTML = '';
+    }
+}
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+const stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
+
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+let resetButton = document.getElementById('reset');   
+resetButton.addEventListener('click', () => stopwatch.reset());
+
+let newRecordButton = document.getElementById('nRecord');
+newRecordButton.addEventListener('click', () => stopwatch.addCheckpoint());
+
+let resetRecordButton = document.getElementById('resetRecords');
+resetRecordButton.addEventListener('click', () => stopwatch.resetRecordList());
+
